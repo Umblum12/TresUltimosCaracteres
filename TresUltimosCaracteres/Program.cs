70a392b01c295784276ff1c821b812877cb27624
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-31 06:00:09 -0600

    Agregar archivos de proyecto.

---
+++ TresUltimosCaracteres/Program.cs
@@ -0,0 +1,5 @@
+﻿string str = "Hola, Bienvenidos a la unidad 3"; string substr = str.Substring(str.Length - 1);string substr1 = str.Substring(str.Length - 3); string substr2 = str.Substring(str.Length - 31, 4); //
+Console.WriteLine(substr);
+Console.WriteLine(substr1);
+Console.WriteLine(substr2);
+Console.ReadKey(true);
